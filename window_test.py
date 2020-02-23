354d58e122252b0646cd81d2d9cbe683fc05e341
robertpyka <rpyka.bytom@gmail.com>
2020-02-23 12:26:25 +0100

    Update window_test.py

--- window_test.py
+++ window_test.py
@@ -4,24 +4,55 @@ import tkinter as tk
 window = tk.Tk()
 
 #adding pictures
-back1 = PhotoImage(file = r"C:\Users\rpyka\Desktop\Nauka programowania\Python\programiki\repozytorium\chess\light.png" )
-back2 = PhotoImage(file = r"C:\Users\rpyka\Desktop\Nauka programowania\Python\programiki\repozytorium\chess\dark.png" )
+back1 = "#FFF8DC" 	
+back2 = "#D2691E"
+e = tk.PhotoImage(width=1, height=1)
+bP = tk.PhotoImage(file = "images/bP.png")
+bR = tk.PhotoImage(file = "images/bR.png")
+bQ = tk.PhotoImage(file = "images/bQ.png")
+bK = tk.PhotoImage(file = "images/bK.png")
+bN = tk.PhotoImage(file = "images/bN.png")
+bB = tk.PhotoImage(file = "images/bB.png")
+tiles={}
+
+pieces = [[bR,bN,bB,bQ,bK,bB,bN,bR],
+        [bP,bP,bP,bP,bP,bP,bP,bP],
+        [e,e,e,e,e,e,e,e],
+        [e,e,e,e,e,e,e,e],
+        [e,e,e,e,e,e,e,e],
+        [e,e,e,e,e,e,e,e],
+        [e,e,e,e,e,e,e,e],
+        [e,e,e,e,e,e,e,e]]
 # creating a chess board
-for i in range(7):
-    for j in range(7):
+for i in range(8):
+    for j in range(8):
         #function if used to set color of button with modulo
         if (i+j)%2 == 0:
             back = back1
         else:
             back = back2
-        button = tk.Button(
+        tiles[(i,j)] = tk.Button(
             master = window,
             relief=tk.FLAT,
             width=80,
             height=80,
-            image = back,
+            bg = back,
+            image= pieces[i][j]
             )
-        button.grid(row=i, column=j)
+        tiles[i,j].grid(row=i, column=j)
+
+# list of poles
+##tiles = [[['a8','b8','c8','d8','e8','f8','g8','h8'],\
+##        ['a7','b7','c7','d7','e7','f7','g7','h7'],\
+##        ['a6','b6','c6','d6','e6','f6','g6','h6'],\
+##        ['a5','b5','c5','d5','e5','f5','g5','h5'],\
+##        ['a4','b4','c4','d4','e4','f4','g4','h4'],\
+##        ['a3','b3','c3','d3','e3','f3','g3','h3'],\
+##        ['a2','b2','c2','d2','e2','f2','g2','h2'],\
+##        ['a1','b1','c1','d1','e1','f1','g1','h1']]]
+# placing pieces
+						
+#placing pieces on board
 
 window. mainloop()
 
